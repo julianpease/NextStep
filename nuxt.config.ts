596222f31ba33a4a26be5704fb27b39ad33f2fce
Julian Pease <julianpease@gmail.com>
2025-06-17 15:09:50 +0200

    fix: updated @nuxt/icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@nuxt/icon'],
   app: {
     head: {
       title: 'Job Listings',
